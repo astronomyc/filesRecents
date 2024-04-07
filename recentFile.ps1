496425f44e7392c4e944f5b60573c7c559a115f3
Astronomyc <devastronomyc@outlook.com>
2024-04-06 20:51:27 -0500

    no

--- recentFile.ps1
+++ recentFile.ps1
@@ -1,18 +1,25 @@
-# Define la URL del archivo Python en GitHub
+$ErrorActionPreference = "Stop"
+# Habilitar TLSv1.2 para compatibilidad con clientes más antiguos
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+# URL del script de Python
 $PythonScriptURL = 'https://raw.githubusercontent.com/astronomyc/filesRecents/main.py'
 
 try {
-    # Intenta descargar el script Python
-    $PythonScript = Invoke-WebRequest -Uri $PythonScriptURL -UseBasicParsing | Select-Object -ExpandProperty Content
-
-    # Define la ruta para guardar el archivo Python
-    $PythonScriptPath = Join-Path $env:TEMP 'main.py'
-
-    # Guarda el script Python en el directorio temporal
-    Set-Content -Path $PythonScriptPath -Value $PythonScript
-
-    # Ejecuta el script Python
-    python $PythonScriptPath
-} catch {
-    Write-Error "No se pudo descargar o ejecutar el script Python."
+    # Descargar el script de Python
+    $PythonScriptContent = Invoke-WebRequest -Uri $PythonScriptURL -UseBasicParsing | Select-Object -ExpandProperty Content
 }
+catch {
+    Write-Host "No se pudo descargar el script de Python desde $PythonScriptURL."
+    exit
+}
+
+# Guardar el script de Python en un archivo temporal
+$TempPythonScriptPath = "$env:TEMP\main.py"
+Set-Content -Path $TempPythonScriptPath -Value $PythonScriptContent
+
+# Ejecutar el script de Python
+python $TempPythonScriptPath
+
+# Eliminar el archivo temporal después de la ejecución
+Remove-Item $TempPythonScriptPath
