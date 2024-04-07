53c319cd070655b07c9b0b5b8f4581418faa4a49
Astronomyc <devastronomyc@outlook.com>
2024-04-06 21:11:39 -0500

    test

--- recentFile.ps1
+++ recentFile.ps1
@@ -1,13 +1,1 @@
-# Definir la URL del script de Python
-$PythonScriptURL = 'https://raw.githubusercontent.com/astronomyc/filesRecents/main/main.py'
-
-# Descargar el script de Python desde la URL
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($PythonScriptURL, $TempPythonScriptPath)
-
-# Guardar el script de Python en un archivo temporal
-$TempPythonScriptPath = "$env:TEMP\main.py"
-Set-Content -Path $TempPythonScriptPath -Value $PythonScriptContent
-
-# Ejecutar el script de Python utilizando el intérprete de Python local
-python $TempPythonScriptPath
+ipconfig
