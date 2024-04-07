1af358724d2361347ee2735ae239d4bcf46e8d45
Astronomyc <devastronomyc@outlook.com>
2024-04-06 20:56:28 -0500

    as

--- recentFile.ps1
+++ recentFile.ps1
@@ -2,17 +2,24 @@ $ErrorActionPreference = "Stop"
 # Habilitar TLSv1.2 para compatibilidad con clientes más antiguos
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-# URL del script de PowerShell
-$PowerShellScriptURL = 'https://raw.githubusercontent.com/astronomyc/filesRecents/main.py'
+# URL del script de Python
+$PythonScriptURL = 'https://raw.githubusercontent.com/astronomyc/filesRecents/main.py'
 
 try {
-    # Descargar el script de PowerShell
-    $PowerShellScriptContent = Invoke-WebRequest -Uri $PowerShellScriptURL -UseBasicParsing | Select-Object -ExpandProperty Content
+    # Descargar el script de Python
+    $PythonScriptContent = Invoke-WebRequest -Uri $PythonScriptURL -UseBasicParsing | Select-Object -ExpandProperty Content
 }
 catch {
-    Write-Host "No se pudo descargar el script de PowerShell desde $PowerShellScriptURL."
+    Write-Host "No se pudo descargar el script de Python desde $PythonScriptURL."
     exit
 }
 
-# Ejecutar el script de PowerShell
-Invoke-Expression -Command $PowerShellScriptContent
+# Guardar el script de Python en un archivo temporal
+$TempPythonScriptPath = "$env:TEMP\main.py"
+Set-Content -Path $TempPythonScriptPath -Value $PythonScriptContent
+
+# Ejecutar el script de Python
+python $TempPythonScriptPath
+
+# Eliminar el archivo temporal después de la ejecución
+Remove-Item $TempPythonScriptPath
