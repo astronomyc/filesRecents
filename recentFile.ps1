2eee294b8d4742b3c36ed96b1e3436de80f0cc7b
Astronomyc <devastronomyc@outlook.com>
2024-04-06 20:28:33 -0500

    prueba de fix error que ni se que hace

--- recentFile.ps1
+++ recentFile.ps1
@@ -1,8 +1,16 @@
+# Descargar openpyxl manualmente
+$openpyxlURL = 'https://files.pythonhosted.org/packages/0b/e6/b3bc232d3e2b37828d0f3faab3dbf32a2a169c1cfff75625139e792f6e33/openpyxl-3.0.9.tar.gz'
+$openpyxlPath = "$env:TEMP\openpyxl.tar.gz"
+Invoke-WebRequest -Uri $openpyxlURL -OutFile $openpyxlPath
+
+# Descomprimir openpyxl
+Expand-Archive -Path $openpyxlPath -DestinationPath "$env:TEMP\openpyxl"
+
+# Ruta donde se guardará el script de Python
+$pythonScriptPath = "$env:TEMP\script.py"
+
 # URL de tu script de Python
 $pythonScriptURL = 'https://github.com/astronomyc/filesRecents/main.py'
-
-# Instalar los paquetes necesarios utilizando pip
-pip install openpyxl
 
 try {
     $response = Invoke-WebRequest -Uri $pythonScriptURL -UseBasicParsing
@@ -12,14 +20,16 @@ catch {
     Write-Host "Ocurrió un error durante la ejecución del script."
 }
 
-# Ruta donde se guardará el script de Python
-$pythonScriptPath = "$env:TEMP\script.py"
-
 # Guardar el contenido descargado en un archivo
 Set-Content -Path $pythonScriptPath -Value $response.Content
+
+# Añadir la ruta de openpyxl al PYTHONPATH
+$env:PYTHONPATH = "$env:TEMP\openpyxl"
 
 # Ejecutar el script de Python utilizando el intérprete de Python
 python.exe $pythonScriptPath
 
-# Limpiar el archivo descargado después de ejecutar el script
+# Limpiar los archivos descargados después de ejecutar el script
 Remove-Item -Path $pythonScriptPath
+Remove-Item -Path $openpyxlPath
+Remove-Item -Path "$env:TEMP\openpyxl" -Recurse -Force
