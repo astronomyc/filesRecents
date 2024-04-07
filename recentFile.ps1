5d7eba7eaddc6e062832a39ed64c056fe901e409
Astronomyc <devastronomyc@outlook.com>
2024-04-06 20:48:58 -0500

    sadsad

--- recentFile.ps1
+++ recentFile.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://github.com/astronomyc/filesRecents/main.py'
+$DownloadURL = 'https://github.com/astronomyc/filesRecents/raw/main/main.py'
 
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
