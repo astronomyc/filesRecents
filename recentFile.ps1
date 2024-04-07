6af3408aa309d44cb172d37e8e05ff8f1a404f83
Astronomyc <devastronomyc@outlook.com>
2024-04-06 21:14:09 -0500

    as

--- recentFile.ps1
+++ recentFile.ps1
@@ -1,5 +1,3 @@
-# Configurar la directiva de ejecución para permitir la ejecución de scripts
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
-
-# Ejecutar el comando ipconfig
+# Contenido del archivo recentFile.ps1
+Write-Output "Mostrando la configuración de red:"
 ipconfig
